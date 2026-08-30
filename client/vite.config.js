9184df2a5beda5c5a49c9ad530658f04dd14c2d6
TheFastest599 <ribhusaha2003@gmail.com>
2026-08-30 19:43:19 +0530

    feat(client): client setup init

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,19 @@
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+	plugins: [
+		react(),
+		tailwindcss(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
